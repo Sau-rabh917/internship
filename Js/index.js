3cef2a86fbb0d34dde93138dc7e41c768242cf99
Saurabh Pandey <pandeyjii917sp@gmail.com>
2025-06-16 12:04:54 +0530

    fifth commit

--- Js/index.js
+++ Js/index.js
@@ -1,3 +1,13 @@
 console.log("hello world");
 console.log("saurabh");
-
+var a = "name";
+// dynimically types variables
+console.log(a);
+ 
+a = "xyz"
+let b = "name";
+// dynimically types variables
+console.log(b);
+ 
+b = 10
+const c = 30;
