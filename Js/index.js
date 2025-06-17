82796fb6c27d04893b930bc88dd57661117dca36
Saurabh Pandey <pandeyjii917sp@gmail.com>
2025-06-17 12:27:27 +0530

    7th commit

--- Js/index.js
+++ Js/index.js
@@ -1,13 +1,64 @@
-console.log("hello world");
-console.log("saurabh");
-var a = "name";
-// dynimically types variables
-console.log(a);
+// console.log("hello world");
+// console.log("saurabh");
+// var a = "name";
+// // dynimically types variables
+// console.log(a);
  
-a = "xyz"
-let b = "name";
-// dynimically types variables
-console.log(b);
+// a = "xyz"
+// let b = "name";
+// // dynimically types variables
+// console.log(b);
  
-b = 10
-const c = 30;
+// b = 10
+// const b = 30;
+
+// var name = "Saurabh"
+// let age = 21
+// const country = "India"
+// console.log(typeof(name));
+// console.log(age);
+// console.log(country);
+// let x = 10; 
+// console.log(typeof(x));
+
+// {
+// let x = 10; 
+// console.log(typeof(x));
+// }
+
+// function scope(){
+//     var a = 10;
+//     console.log(a);
+    
+// }
+ 
+// scope();
+
+
+//  let a=10;
+//  a = 20;
+
+// console.log(a);
+
+
+//  var b=10;
+//  b = 20;
+
+// console.log(b);
+
+// const c=10;
+
+
+// console.log(c);
+
+let fruits = ["Apple", "Banana", "Mango",10];
+fruits.shift();
+fruits.unshift("gg");
+fruits.pop();
+fruits.push("hh");
+console.log(fruits);
+
+
+
+
+
