853a2632479f4c83f17c2bd5c2056983609e863b
Saurabh Pandey <pandeyjii917sp@gmail.com>
2025-06-20 13:02:02 +0530

    react addedd

--- react/react-learning/src/App.jsx
+++ react/react-learning/src/App.jsx
@@ -6,7 +6,7 @@ const App = () => {
   }
   return (
     <>
-      <h1>Information</h1>
+      <h1 className='text-center text-5xl text-orange-500 mt-0 border-blue-200 border-4'>Information</h1>
       <p>My Name is Saurabh Pandey .. </p>
       <button onClick={TextChange}>click me</button>
     </>
