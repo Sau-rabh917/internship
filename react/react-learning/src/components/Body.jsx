99ac0368106252a99caa3f228d529357835c78d4
Saurabh Pandey <pandeyjii917sp@gmail.com>
2025-06-28 12:14:22 +0530

    api learning

--- react/react-learning/src/components/Body.jsx
+++ react/react-learning/src/components/Body.jsx
@@ -5,20 +5,11 @@ const Body = () => {
   const add = () => setnumber(number + 1);
   
   const subtract = () =>setnumber(number - 1);
-
+63
 
   return (
     <>
-    <div className="w-full h-screen flex flex-col items-center gap-20">
-        
-        <button className="bg-blue-600 p-2 text-white rounded-lg " onClick={add}>
-          Increase Number
-        </button>
-        <span className="text-4xl">{number}</span>
-        <button className="bg-orange-800 p-2 text-white rounded-lg" onClick={subtract}>
-          Decrease Number
-        </button>
-      </div>
+    
       
         
       
